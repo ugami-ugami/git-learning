4027b9dbe3e1088c5f8270054611bc2877833f19
anlagunov <an.lagunov@korona.net>
2025-10-02 10:11:40 +0300

    amend commit

---
+++ src/two/function/practice/TwoFileForAmend.java
@@ -0,0 +1,7 @@
+package two.function.practice;
+
+public class TwoFileForAmend {
+    public static void main (String[] args) {
+        System.out.println("TwoFileForAmend");
+    }
+}
---
+++ src/two/function/practice/TwoFunctionPractice.java
@@ -0,0 +1,12 @@
+package two.function.practice;
+
+public class TwoFunctionPractice {
+
+    public static void pryntName (String name) {
+        System.out.println("Привет, " + name);
+    }
+
+    public static void main (String[] args) {
+        pryntName("Lexa");
+    }
+}
