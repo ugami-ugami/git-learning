049681647c6e0b7fd83713c8d20056dcabb41f25
anlagunov <an.lagunov@korona.net>
2025-10-02 14:17:25 +0300

    updat v2 new message

--- src/two/function/practice/TwoFileForAmend.java
+++ src/two/function/practice/TwoFileForAmend.java
@@ -1,7 +1,16 @@
 package two.function.practice;
 
 public class TwoFileForAmend {
+
+    public static boolean tOf (int a, int b) {
+        if (a > b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     public static void main (String[] args) {
-        System.out.println("TwoFileForAmend");
+       System.out.println(tOf(4, 3));
     }
 }
